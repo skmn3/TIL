27bd0c65541decab3dafcbaa123eb4ec96324c4a
Sangjin <sangjin170202@naver.com>
2022-07-14 15:01:18 +0900

    📑 :: Variable

---
+++ Python/basic/variable(변수).py
@@ -0,0 +1,17 @@
+# 변수
+
+animal = "강아지"
+name = "판다"
+age = 5
+hobby = "산책"
+is_adult = age > 19
+
+'''
+저는 뽀로로에여
+'''
+
+print("우리 집에는 "+ animal + "가 있다.")
+print("그 "+ animal + "의 이름은 "+ name + "이다")
+# print(name + "는 "+ str(age) + "살이며, "+ hobby + "을 좋아한다")
+print(name,"는",str(age), "살이며,",hobby,"을 좋아한다")
+print(name + "는 어른일까요?? "+ str(is_adult))
