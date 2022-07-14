54e74979dfb28e5873ab44f0ef47ebfee3786eed
Sangjin <sangjin170202@naver.com>
2022-07-14 15:00:13 +0900

    📑 :: Formula Processing Function

---
+++ Python/basic/formula processing function(수식처리함수).py
@@ -0,0 +1,16 @@
+# 수식 처리 함수
+
+print(abs(-5)) # abs => 절댓값 => 5
+print(pow(abs(-5), abs(-4))) # -5의 절댓값^-4의 절댓값 => 5 * 5 * 5 * 5 =625
+
+print(max(5, 4)) # 최댓값
+print(min(5, 4)) # 최솟값
+
+print(round(5.54)) # 반올림
+print(round(5.12)) 
+
+from math import *
+
+print(floor(4.22)) # 내림
+print(ceil(4.12545)) # 올림
+print(sqrt(4)) # 제곱근
