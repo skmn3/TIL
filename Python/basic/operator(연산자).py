15bbad4d19799c0e19d5955284ad14054060549b
Sangjin <sangjin170202@naver.com>
2022-07-14 15:01:37 +0900

    📑 :: Operator

---
+++ Python/basic/operator(연산자).py
@@ -0,0 +1,38 @@
+# 연산자
+
+print(1+1) #2
+print(3-2) #1
+print(5*2) #10
+print(6/3) #2
+
+print(2**3) # 2^3 => 8
+print(5%3) # 2
+print(10%3) # 1
+print(5//3) # 1
+print(10//3) # 3
+
+print(10 > 3) # True
+print(4 > 10) # False
+print(4 >= 3) # True
+print(4 <= 3) # False
+
+print(4 == 4) # True
+print(4 == 2) # False
+
+print(5 != 6) # True
+print(5 != 5) # False
+
+print(not(1 != 6)) # False
+print(not(1 != 1)) # True
+
+print((3 > 0) and (5 > 4)) # True
+print((3 > 0) & (5 < 4)) # False
+print((3 < 0) or (5 < 4)) # False
+print((3 < 0) | (5 > 4)) # True
+
+print(not(1 != 6) | (1 != 6)) # False
+
+print( 9 < 5 > 9) # False
+
+
+
