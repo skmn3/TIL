90380b8f1a59bb19905af847153eaf6cebcda68f
Sangjin <sangjin170202@naver.com>
2022-07-15 15:24:51 +0900

    📑 :: Tuple

---
+++ Python/basic/tuple(튜플).py
@@ -0,0 +1,18 @@
+# 튜플
+
+Coding_language = ("Java", "Python", "Go", "C", "C++")
+
+print(Coding_language[0])
+
+# Coding_language.add("TypeScript") # 튜플에 add는 없음
+
+name = "뽀로로"
+age = 1
+hobby = "코딩"
+print(name, age, hobby)
+
+name_1, age_1, hobby_1 = "뽀로로", 1, "코딩"
+print(name_1, age_1, hobby_1)
+
+(name_2, age_2, hobby_2) = ("뽀로로", 1, "코딩")
+print(name_2, age_2, hobby_2)
