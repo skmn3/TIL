4938f55b5d9066b0ba4967fd37f4aafb12b2082d
Sangjin <sangjin170202@naver.com>
2022-07-16 16:10:59 +0900

    📑 :: Stack

---
+++ Java/basic/day_0620/a2/Coin.java
@@ -0,0 +1,15 @@
+package Java.basic.day_0620.a2;
+
+public class Coin {
+
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+}
