9cf9d73996becee235d11bac833a307f5adbf481
Sangjin <sangjin170202@naver.com>
2022-07-16 17:19:34 +0900

    📑 :: Mathematical Expression

---
+++ Python/basic/mathematical expression(수식 표현).py
@@ -0,0 +1,26 @@
+# 수식 표현
+
+print(2 + 9 * 4) # 38
+print((2+9) * 4) # 44
+
+number = 2 + 9 * 4
+print(number)
+
+number = number + 9
+print(number)
+
+number += 25
+print(number)
+
+number -= 20
+print(number)
+
+number *= 30
+print(number)
+
+number /= 100
+print(number)
+
+number %= 100
+print(number)
+
