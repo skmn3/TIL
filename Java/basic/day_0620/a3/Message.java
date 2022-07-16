ae4650e8eaed953fbb5f236f7f354fa731853568
Sangjin <sangjin170202@naver.com>
2022-07-16 15:47:50 +0900

    📑 :: Queue

---
+++ Java/basic/day_0620/a3/Message.java
@@ -0,0 +1,13 @@
+package Java.basic.day_0620.a3;
+
+public class Message {
+
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+	}
+
+}
---
+++ Java/basic/day_0620/a3/QueueExample.java
@@ -0,0 +1,32 @@
+package Java.basic.day_0620.a3;
+
+import java.util.*;
+
+public class QueueExample {
+	
+	@SuppressWarnings("unused")
+	private static boolean offer;
+
+	public static void main(String[] args) {
+		Queue<Message> messageQueue = new LinkedList<Message>();
+		
+		messageQueue.offer(new Message("sendMail", "홍길동"));
+		messageQueue.offer(new Message("sendSMS", "신용권"));
+		messageQueue.offer(new Message("sendKakaoTalk", "나나나나"));
+		
+		while (!messageQueue.isEmpty()) {
+			Message message = messageQueue.poll();
+			switch (message.command) {
+			case "sendMail":
+				System.out.println(message.to + "님에게 메일을 보냅니다.");
+			case "sendSMS":
+				System.out.println(message.to + "님에게 SMS을 보냅니다.");
+				break;
+			case "sendKakaoTalk":
+				System.out.println(message.to + "님에게 카카오톡을 보냅니다.");
+				break;
+			}	
+		}
+	}
+
+}
