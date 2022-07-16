5cdd5ef0aba2204ce814735e4b3ed06e65548cf9
Sangjin <sangjin170202@naver.com>
2022-07-16 16:55:04 +0900

    📑 :: Inheritance

---
+++ Java/basic/day_0420/no2/Tv.java
@@ -0,0 +1,33 @@
+package Java.basic.day_0420.no2;
+
+public class Tv implements RemoteControl {
+	
+	// 필드
+	private int volume;
+	
+	// turnOn() 추상 메소드의 실체 메소드 (재정의)
+	@Override
+	public void turnOn() {
+		System.out.println("Tv를 킵니다");
+	}
+	
+	// turnOff() 추상 메소드의 실체 메소드 (재정의)
+	@Override
+	public void turnOff() {
+		System.out.println("Tv를 끕니다");
+	}
+	
+	// setVolume() 추상 메소드의 실체 메소드
+	@Override
+	public void setVolume(int volume) {
+		if(volume > RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if(volume < RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 Tv 볼륨: " + this.volume);
+	}
+
+}
