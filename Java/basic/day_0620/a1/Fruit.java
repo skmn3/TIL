6e8e5f86e30028b703af32b053c99fbd6ae62619
Sangjin <sangjin170202@naver.com>
2022-07-16 16:12:05 +0900

    📑 :: Descending

---
+++ Java/basic/day_0620/a1/Fruit.java
@@ -0,0 +1,13 @@
+package Java.basic.day_0620.a1;
+
+public class Fruit {
+
+	public String name;
+	public int price;
+	
+	public Fruit(String name,int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+}
